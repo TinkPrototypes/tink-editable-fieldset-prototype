fa4a148441dbda20bf0920ce866bb6652c5aee98
Jasper Van Proeyen <trianglejuice@mac.com>
2015-08-17 14:26:55 +0200

    Started v2

--- src/scripts/directives/fieldset-editable.js
+++ src/scripts/directives/fieldset-editable.js
@@ -72,45 +72,68 @@ angular.module('tink.fieldsetEditable', [])
           });
 
 
-          // Fields can be edited now
-          function enableEditMode(event) {
-            $(element).addClass('fieldset-editable-is-active');
-            scope.editModeActive = true;
-            var form = $(element).closest('form');
-            scope.originalContents = $(form).serializeArray();
-            // console.log(scope.originalContents);
-            window.setTimeout (function(){
-              event.currentTarget.select();
-            }, 10);
-            $(element).removeClass('fieldset-editable-is-hovered');
-          }
+          // Show input field when link is clicked
+          $('.anchor-add').click(function() {
+            $('.anchor-add').toggle(0);
+            $('.input-add').toggle(0);
+            $(this).next().find(':input').focus();
+          });
 
-
-          // Check if the user cancelled editing or not
-          function disableEditMode(saveChanges) {
-            saveChanges = typeof saveChanges !== 'undefined' ? saveChanges : false;
-
-            if(saveChanges) {
-              $('input').blur();
-              console.log('Changes saved!');
-            } else {
-              resetValues(scope.originalContents);
-              console.log('Operation cancelled!');
-            }
-
-            if($('input:focus').length === 0) {
-              scope.editModeActive = false;
-              $(element).removeClass('fieldset-editable-is-active');
-              $('.btn-submit').prop('disabled', true);
-            }
-          }
         });
 
 
+        /**
+         * Functions
+         * ----------------------------------------------------------------
+         */
+
+        // Fields can be edited now
+        function enableEditMode(event) {
+          $(element).addClass('fieldset-editable-is-active');
+          scope.editModeActive = true;
+          var form = $(element).closest('form');
+          scope.originalContents = $(form).serializeArray();
+          // console.log(scope.originalContents);
+          window.setTimeout (function(){
+            event.currentTarget.select();
+          }, 10);
+          $(element).removeClass('fieldset-editable-is-hovered');
+        }
+
+
+        // Check if the user cancelled editing or not
+        function disableEditMode(saveChanges) {
+          saveChanges = typeof saveChanges !== 'undefined' ? saveChanges : false;
+          var els = $(element).find(':input').get();
+          // console.log(els);
+          $.each(els, function() {
+            if(this.value === '' && this.type !== 'submit' && $(this).parents('.input-add').length > 0) {
+              $(this).parents('.input-add').toggle(0);
+              $(this).parents('.input-add').prev().toggle(0);
+            }
+          });
+
+          if(saveChanges) {
+            $('input').blur();
+            console.log('Changes saved!');
+          } else {
+            resetValues(scope.originalContents, els);
+            console.log('Operation cancelled!');
+          }
+
+
+          // Primary buttons
+          if($('input:focus').length === 0) {
+            scope.editModeActive = false;
+            $(element).removeClass('fieldset-editable-is-active');
+            $('.btn-submit').prop('disabled', true);
+          }
+        }
+
+
         // Reset values
-        function resetValues(data) {
+        function resetValues(data, els) {
           if(!$('.btn-submit').prop('disabled')) {
-            var els = $(element).find(':input').get();
 
             var obj = {}; // Convert the serializeArray into an accesible object
             for (var i = 0, l = data.length; i < l; i++) {
